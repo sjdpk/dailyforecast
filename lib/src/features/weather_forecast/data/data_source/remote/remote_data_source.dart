8f9f7642bea3b874bc213135ffdc99881e2bb4ef
Deepak Sapkota <iamdpk.spk@gmail.com>
2023-08-10 10:44:23 +0545

    feat(data): featch curretn-weather and forecast data from api

---
+++ lib/src/features/weather_forecast/data/data_source/remote/remote_data_source.dart
@@ -0,0 +1,31 @@
+import 'package:dailyforecast/main.dart';
+import 'package:http/http.dart' as http;
+
+abstract class RemoteWeatherDataSource {
+  Future<http.Response> getCurrentWeather({required num lat, required num lon});
+  Future<http.Response> getweatherForecastList({required num locationId});
+}
+
+class RemoteDataSourceImpl implements RemoteWeatherDataSource {
+  final http.Client _client;
+  RemoteDataSourceImpl(this._client);
+
+  final String baseUrl = appEnv.envConfig.baseUrl;
+  final String appId = appEnv.envConfig.weatherApiKey;
+
+  // @desc :  get current weather conditon based on location latitude and longitude
+  // @return : http Response [statusCode,request, body ...]
+  @override
+  Future<http.Response> getCurrentWeather({required num lat, required num lon}) async {
+    final response = await _client.get(Uri.parse("$baseUrl/weather?lat=$lat&lon=$lon&appid=$appId"));
+    return response;
+  }
+
+  // @desc :  get 5day weather foirecast based on location id
+  // @return : http Response [statusCode,request, body ...]
+  @override
+  Future<http.Response> getweatherForecastList({required num locationId}) async {
+    final response = await _client.get(Uri.parse("$baseUrl/forecast?id=$locationId&appid=$appId"));
+    return response;
+  }
+}
