5a69e8694dd4c466a78150d7999f164171734e8b
Deepak Sapkota <iamdpk.spk@gmail.com>
2023-08-10 16:17:30 +0545

    fix : WeatherErrorState issue

--- lib/src/features/weather_forecast/presentation/blocs/location/location_bloc.dart
+++ lib/src/features/weather_forecast/presentation/blocs/location/location_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:dailyforecast/src/core/network/data_state.dart';
 import 'package:dailyforecast/src/features/weather_forecast/domain/entities/location.dart';
 import 'package:dailyforecast/src/features/weather_forecast/domain/usecases/remote/getlocation.dart';
-import 'package:dailyforecast/src/features/weather_forecast/presentation/blocs/currentweather/weather_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,8 +18,8 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
     final locations = await _getLocationList.execute(query: query);
     if (locations is DataSucessState) {
       emit(LocationDoneState(locations.data!));
-    } else {
-      emit(const WeatherErrorState("some thing went wrong"));
+    } else if (locations is DataErrorState) {
+      emit(const LocationErrorState("some thing went wrong"));
     }
   }
 }
