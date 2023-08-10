176467bc1f2a21f8cd760c22ef27760d84abaad6
Deepak Sapkota <iamdpk.spk@gmail.com>
2023-08-10 10:50:46 +0545

    feat(data) : implement weather-repo to weather usecase

---
+++ lib/src/features/weather_forecast/data/repository/weather_repo_impl.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dailyforecast/src/core/network/data_state.dart';
+import 'package:dailyforecast/src/features/weather_forecast/data/data_source/remote/remote_data_source.dart';
+import 'package:dailyforecast/src/features/weather_forecast/data/models/weather_model.dart';
+import 'package:dailyforecast/src/features/weather_forecast/domain/entities/weather.dart';
+import 'package:dailyforecast/src/features/weather_forecast/domain/repository/weather_repo.dart';
+
+class WeatherRepositoryImpl implements WeatherRepository {
+  final RemoteWeatherDataSource _remoteWeatherDataSource;
+  const WeatherRepositoryImpl(this._remoteWeatherDataSource);
+
+  // @desc : get current weather condn from weatherRepository (remote) 
+  // @return : DatsState with sucess || error case
+  @override
+  Future<DataState<WeatherEntity>> getCurrentWeather({required num lat, required num lon}) async {
+    try {
+      final response = await _remoteWeatherDataSource.getCurrentWeather(lat: lat, lon: lon);
+      final jsonData = json.decode(response.body);
+
+      if (response.statusCode == HttpStatus.ok) {
+        final weatherModelData = WeatherModel.fromJson(jsonData);
+        return DataSucessState(weatherModelData.toEntity());
+      } else {
+        return DataErrorState(jsonData["message"]);
+      }
+    } on SocketException {
+      return const DataErrorState("no internet connection");
+    } catch (e) {
+      return DataErrorState(e.toString());
+    }
+  }
+
+  // @desc : get location 5days forecast list from weatherRepository (remote) 
+  // @return : DatsState with sucess || error case
+  @override
+  Future<DataState<List<WeatherEntity>>> getweaterForecastList({required num locationId}) async {
+    try {
+      final response = await _remoteWeatherDataSource.getweatherForecastList(locationId: locationId);
+      final jsonData = json.decode(response.body);
+
+      if (response.statusCode == HttpStatus.ok) {
+        List<WeatherModel> weatherModelData = jsonData['list'].map<WeatherModel>((dynamic i) => WeatherModel.fromJson(i as Map<String, dynamic>)).toList();
+        return DataSucessState(weatherModelData);
+      } else {
+        return DataErrorState(jsonData["message"]);
+      }
+    } on SocketException {
+      return const DataErrorState("no internet connection");
+    } catch (e) {
+      return DataErrorState(e.toString());
+    }
+  }
+}
