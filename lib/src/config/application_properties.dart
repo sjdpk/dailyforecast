64ec64d4c8d1da2eaf26a6d06ec429507324b38b
Deepak Sapkota <iamdpk.spk@gmail.com>
2023-08-10 10:22:47 +0545

    feat: add files for different environments (prod, dev, local)

---
+++ lib/src/config/application_properties.dart
@@ -0,0 +1,3 @@
+import 'package:dailyforecast/src/core/env/base_env.dart';
+
+const applicationEnv = Environment.dev;
---
+++ lib/src/core/env/local_env.dart
@@ -0,0 +1,27 @@
+import 'base_env.dart';
+
+class LocalEnvironment implements BaseEnvironment {
+  @override
+  // TODO: implement applicatioName
+  String get applicatioName => throw UnimplementedError();
+
+  @override
+  // TODO: implement baseurl
+  String get baseUrl => throw UnimplementedError();
+
+  @override
+  // TODO: implement weatherApiKey
+  String get weatherApiKey => throw UnimplementedError();
+
+  @override
+  // TODO: implement openStreetUrl
+  String get openStreetUrl => throw UnimplementedError();
+
+  @override
+  // TODO: implement iconUrl
+  String get iconUrl => throw UnimplementedError();
+  
+  @override
+  // TODO: implement locationDb
+  String get locationDb => throw UnimplementedError();
+}
